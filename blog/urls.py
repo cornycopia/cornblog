6524b994a6e09a444398a0ef93f23a91d026c7a6
cornycopia <lorenzo.b.aquino@gmail.com>
2016-11-24 00:05:10 +0800

    Updating urls/views to working entry version

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,4 @@ app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<entry_id>[0-9]+)/$', views.entry, name='entry'),
-    url(r'^(?P<entry_id>[0-9]+)/comment/$', views.post_comment, name='post_comment'),
-    url(r'^(?P<entry_id>[0-9]+)/upvote/$', views.upvote, name='upvote'),
-    url(r'^(?P<entry_id>[0-9]+)/downvote/$', views.downvote, name='downvote'),
-    #url(r'^(?P<entry_id>[0-9]+)/(?P<comment_id>[0-9]+)/upvote/$', views.comm_upvote, name='comm_upvote'),
-    #url(r'^(?P<entry_id>[0-9]+)/(?P<comment_id>[0-9]+)/downvote/$', views.comm_downvote, name='comm_downvote'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 from django.utils import timezone
 
 from .models import Entry, Comment
+from .forms import EntryForm
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -15,48 +16,23 @@ class IndexView(generic.ListView):
         """Return the last five published blog entries."""
         return Entry.objects.order_by('-score')[:5]
 
-def upvote(request, entry_id):
-    entry = get_object_or_404(Entry, pk=entry_id)
-    entry.score += 1
-    entry.save()
-    return render(request, 'blog/upvote.html', {'entry' : entry})
-
-def downvote(request, entry_id):
-    entry = get_object_or_404(Entry, pk=entry_id)
-    entry.score -= 1
-    entry.save()
-    return render(request, 'blog/downvote.html', {'entry' : entry})
-
-"""
-def comm_upvote(request, comment_id):
-    comment = get_object_or_404(Comment, pk=comment_id)
-    comment.score += 1
-    comment.save()
-    return render(request, 'blog/comment_upvote.html', {'comment' : comment})
-
-def comm_downvote(request, comment_id):
-    comment = get_object_or_404(Comment, pk=comment_id)
-    comment.score -= 1
-    comment.save()
-    return render(request, 'blog/comment_downvote.html', {'comment' : comment})
-"""
-
 def entry(request, entry_id):
     entry = get_object_or_404(Entry, pk=entry_id)
-    return render(request, 'blog/entry.html', {'entry' : entry})
-
-def post_comment(request, entry_id):
-    entry = get_object_or_404(Entry, pk=entry_id)
-    try:
-        i_commenter = request.POST['commenter']
-        i_comment_text = request.POST['comment_text']
-    except (KeyError, Comment.DoesNotExist):
-        return render(request, 'blog/post_comment.html', {
-            'entry' : entry,
-            'error_message': "There is an error during the comment creation",
-        })
-    else:
-        entry.save()
-        entry.comment_set.create(pub_time=timezone.now(), commenter=i_commenter, comment_text=i_comment_text)
+    form = EntryForm()
+    if request.method == 'POST':
+        action = request.POST['action']
+        if action == 'upvote':
+            entry.score += 1
+            entry.save()
+        elif action == 'downvote':
+            entry.score -= 1
+            entry.save()
+        else:
+            form = EntryForm(request.POST)
+            if form.is_valid():
+                i_commenter = form.cleaned_data['commenter']
+                i_comment_text = form.cleaned_data['comment']
+                entry.save()
+                entry.comment_set.create(pub_time=timezone.now(), commenter=i_commenter, comment_text=i_comment_text)
         return HttpResponseRedirect(reverse('blog:entry', args=(entry.id,)))
-
+    return render(request, 'blog/entry.html', {'entry' : entry, 'form' : form})
